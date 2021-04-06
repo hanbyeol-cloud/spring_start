df48eff97f048c7d5e47b7d32f0ca6cee07eaa07
hanbyeol-cloud <gksqufzp@naver.com>
2021-04-06 17:49:49 +0900

    4월6일 스프링 데이터 JPA까지

--- src/test/java/hello/hellospring/service/MemberServiceIntegrationTest.java
+++ src/test/java/hello/hellospring/service/MemberServiceIntegrationTest.java
@@ -15,7 +15,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @SpringBootTest
-//@Transactional
+@Transactional
 class MemberServiceIntegrationTest {
 
     @Autowired MemberService memberService;
