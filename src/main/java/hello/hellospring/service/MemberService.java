9d021e90a625520c1b3810a705f1b107b08fb119
hanbyeol-cloud <gksqufzp@naver.com>
2021-04-07 17:34:24 +0900

    4월7일 완료

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -23,12 +23,12 @@ public class MemberService {
     //회원 가입
 
     public Long join(Member member) {
-        //같은 이름이 있는 중복 회원은 불가
-        validateDuplicateMember(member);
-        memberRepository.save(member);
-        return member.getId();
-    }
 
+            //같은 이름이 있는 중복 회원은 불가
+            validateDuplicateMember(member);
+            memberRepository.save(member);
+            return member.getId();
+    }
     private void validateDuplicateMember(Member member) {
         memberRepository.findByName(member.getName())
                 .ifPresent(m -> {
@@ -38,7 +38,8 @@ public class MemberService {
 
     //전체 회원 조회
     public List<Member> findMembers() {
-        return memberRepository.findAll();
+
+            return memberRepository.findAll();
     }
 
     public Optional<Member> findOne(Long memberId) {
